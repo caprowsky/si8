dba7aa868a03d665f4e402c2a70b70fe37d650e6
caprowsky <alessandrocapra@gmail.com>
2017-05-27 18:46:25 +0200

    aggiungo i json di notizie nelle tre lingue

--- download.sh
+++ download.sh
@@ -1,5 +1,30 @@
 #!/usr/bin/env bash
 
+# Utenti 
 wget http://localhost:8888/it/api/si7_utenti.json -O modules/custom/migrando/files/si8_utenti.json
 sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_utenti.json
 echo "}" >> modules/custom/migrando/files/si8_utenti.json
+
+#
+#       NOTIZIE
+#
+
+# Notizie in italiano
+wget http://localhost:8888/it/api/si7_notizia_it.json -O modules/custom/migrando/files/si8_notizia_it.json
+sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_notizia_it.json
+echo "}" >> modules/custom/migrando/files/si8_notizia_it.json
+
+
+# Notizie in inglese
+wget http://localhost:8888/it/api/si7_notizia_en.json -O modules/custom/migrando/files/si8_notizia_en.json
+sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_notizia_en.json
+echo "}" >> modules/custom/migrando/files/si8_notizia_en.json
+
+# Notizie in spagnolo
+wget http://localhost:8888/it/api/si7_notizia_es.json -O modules/custom/migrando/files/si8_notizia_es.json
+sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_notizia_es.json
+echo "}" >> modules/custom/migrando/files/si8_notizia_es.json
+
+#
+#       EVENTI
+#
