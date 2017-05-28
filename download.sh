271b82a18497729b67b5ee2f8f7d0f0343ba3b7f
caprowsky <alessandrocapra@gmail.com>
2017-05-28 11:45:06 +0200

    aggiunti i due json per Pagina statica uno per i TNID pieni e uno vuoti

--- download.sh
+++ download.sh
@@ -10,9 +10,9 @@ echo "}" >> modules/custom/migrando/files/si8_utenti.json
 #
 
 # Notizie in italiano
-#wget http://localhost:8888/it/api/si7_notizia_it.json -O modules/custom/migrando/files/si8_notizia_it.json
-#sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_notizia_it.json
-#echo "}" >> modules/custom/migrando/files/si8_notizia_it.json
+wget http://localhost:8888/it/api/si7_notizia_it.json -O modules/custom/migrando/files/si8_notizia_it.json
+sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_notizia_it.json
+echo "}" >> modules/custom/migrando/files/si8_notizia_it.json
 
 
 # Notizie in inglese
@@ -26,5 +26,15 @@ sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_notizia_es.json
 echo "}" >> modules/custom/migrando/files/si8_notizia_es.json
 
 #
-#       EVENTI
+#       PAGINE STATICHE
 #
+
+# Pagine statiche in IT EN ES con TNID > 0
+wget http://localhost:8888/it/api/si7_pagina_it_en_es_tnid1.json -O modules/custom/migrando/files/si8_pagina_it_en_es_tnid1.json
+sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_pagina_it_en_es_tnid1.json
+echo "}" >> modules/custom/migrando/files/si8_pagina_it_en_es_tnid1.json
+
+# Pagine statiche in IT EN ES con TNID = 0
+wget http://localhost:8888/it/api/si7_pagina_it_en_es_tnid0.json -O modules/custom/migrando/files/si8_pagina_it_en_es_tnid0.json
+sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_pagina_it_en_es_tnid0.json
+echo "}" >> modules/custom/migrando/files/si8_pagina_it_en_es_tnid0.json
