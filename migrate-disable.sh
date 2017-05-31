c92349ccb7d2d55ea56e653431cd46db8ad84283
Sergio Durzu <sergio.durzu@gmail.com>
2017-05-31 16:47:22 +0200

    Added migrate-reset and migrate-disable script

--- migrate-disable.sh
+++ migrate-disable.sh
@@ -2,4 +2,3 @@
 
 drush pm-uninstall migrando -y; drush pm-uninstall migrate_tools -y; drush pm-uninstall migrate_plus -y; drush pm-uninstall migrate_drupal -y; drush pm-uninstall migrate -y
 
-drush en migrando -y
