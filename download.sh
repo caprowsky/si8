de461033d20b69677e1530f00a1a2b635d0b6f5b
caprowsky <alessandrocapra@gmail.com>
2017-06-11 12:19:30 +0200

    completati tutti i json per tutti i contenuti termini e files

--- download.sh
+++ download.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-# Utenti 
-#wget http://localhost:8888/it/api/si7_utenti.json -O modules/custom/migrando/files/si8_utenti.json
-#sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_utenti.json
-#echo "}" >> modules/custom/migrando/files/si8_utenti.json
+#
+#       IMPORT CONTENUTI MAPPATI SU CONTENT TYPE "NEWS"
+#
 
 #
 #       NOTIZIE
@@ -14,12 +13,10 @@
 #sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_notizia_tnid_0.json
 #echo "}" >> modules/custom/migrando/files/si8_notizia_tnid_0.json
 
-
 # Notizie con tnid no 0
 #wget http://localhost:8888/en/api/si7_notizia_tnid_no_0.json -O modules/custom/migrando/files/si8_notizia_tnid_no_0.json
 #sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_notizia_tnid_no_0.json
 #echo "}" >> modules/custom/migrando/files/si8_notizia_tnid_no_0.json
-
 
 #
 #       EVENTO
@@ -30,48 +27,91 @@ wget http://localhost:8888/en/api/si7_evento_tnid_0.json -O modules/custom/migra
 sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_evento_tnid_0.json
 echo "}" >> modules/custom/migrando/files/si8_evento_tnid_0.json
 
-
 # Evento con tnid no 0
 wget http://localhost:8888/en/api/si7_evento_tnid_no_0.json -O modules/custom/migrando/files/si8_evento_tnid_no_0.json
 sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_evento_tnid_no_0.json
 echo "}" >> modules/custom/migrando/files/si8_evento_tnid_no_0.json
 
+#
+#       SARDEGNASUAP NOTIZIA
+#
+# Sardegnasuap notizia con tnid = 0
+wget http://localhost:8888/en/api/si7_sardegnasuapnotizia_tnid_0.json -O modules/custom/migrando/files/si8_sardegnasuapnotizia_tnid_0.json
+sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_sardegnasuapnotizia_tnid_0.json
+echo "}" >> modules/custom/migrando/files/si8_sardegnasuapnotizia_tnid_0.json
+
+# Sardegnasuap notizia con tnid no 0
+wget http://localhost:8888/en/api/si7_sardegnasuapnotizia_tnid_no_0.json -O modules/custom/migrando/files/si8_sardegnasuapnotizia_tnid_no_0.json
+sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_sardegnasuapnotizia_tnid_no_0.json
+echo "}" >> modules/custom/migrando/files/si8_sardegnasuapnotizia_tnid_no_0.json
+
+#####################################################################################################
+
+#
+#       IMPORT CONTENUTI MAPPATI SU CONTENT TYPE "PAGE"
+#
+
 
 #
 #       ARTICOLO
 #
-
 # Articolo con tnid = 0
 wget http://localhost:8888/en/api/si7_articolo_tnid_0.json -O modules/custom/migrando/files/si8_articolo_tnid_0.json
 sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_articolo_tnid_0.json
 echo "}" >> modules/custom/migrando/files/si8_articolo_tnid_0.json
 
-
 # Articolo con tnid no 0
 wget http://localhost:8888/en/api/si7_articolo_tnid_no_0.json -O modules/custom/migrando/files/si8_articolo_tnid_no_0.json
 sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_articolo_tnid_no_0.json
 echo "}" >> modules/custom/migrando/files/si8_articolo_tnid_no_0.json
+#
+#       PAGINA STATICA
+#
+
+# Pagina statica con tnid = 0
+wget http://localhost:8888/en/api/si7_paginastatica_tnid_0.json -O modules/custom/migrando/files/si8_paginastatica_tnid_0.json
+sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_paginastatica_tnid_0.json
+echo "}" >> modules/custom/migrando/files/si8_paginastatica_tnid_0.json
+
+# Pagina statica con tnid no 0
+wget http://localhost:8888/en/api/si7_paginastatica_tnid_no_0.json -O modules/custom/migrando/files/si8_paginastatica_tnid_no_0.json
+sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_paginastatica_tnid_no_0.json
+echo "}" >> modules/custom/migrando/files/si8_paginastatica_tnid_no_0.json
 
 #
-#       SARDEGNASUAP NOTIZIA
+#       SARDEGNA SUAP PAGINA
 #
 
-# Sardegnasuap notizia con tnid = 0
-wget http://localhost:8888/en/api/si7_sardegnasuap_tnid_0.json -O modules/custom/migrando/files/si8_sardegnasuap_tnid_0.json
-sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_sardegnasuap_tnid_0.json
-echo "}" >> modules/custom/migrando/files/si8_sardegnasuap_tnid_0.json
+# Sardegna Suap pagina con tnid = 0
+wget http://localhost:8888/en/api/si7_sardegnasuappagina_tnid_0.json -O modules/custom/migrando/files/si8_sardegnasuappagina_tnid_0.json
+sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_sardegnasuappagina_tnid_0.json
+echo "}" >> modules/custom/migrando/files/si8_sardegnasuappagina_tnid_0.json
 
+# Sardegna Suap pagina con tnid no 0
+wget http://localhost:8888/en/api/si7_sardegnasuappagina_tnid_no_0.json -O modules/custom/migrando/files/si8_sardegnasuappagina_tnid_no_0.json
+sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_sardegnasuappagina_tnid_no_0.json
+echo "}" >> modules/custom/migrando/files/si8_sardegnasuappagina_tnid_no_0.json
 
-# Sardegnasuap notizia con tnid no 0
-wget http://localhost:8888/en/api/si7_sardegnasuap_tnid_no_0.json -O modules/custom/migrando/files/si8_sardegnasuap_tnid_no_0.json
-sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_sardegnasuap_tnid_no_0.json
-echo "}" >> modules/custom/migrando/files/si8_sardegnasuap_tnid_no_0.json
+#####################################################################################################
 
-
+#       FAQ
 #
+wget http://localhost:8888/en/api/si7_faq.json -O modules/custom/migrando/files/si8_faq.json
+sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_faq.json
+echo "}" >> modules/custom/migrando/files/si8_faq.json
+
+#####################################################################################################
+
+#       GLOSSARIO
+#
+wget http://localhost:8888/en/api/si7_glossario.json -O modules/custom/migrando/files/si8_glossario.json
+sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_glossario.json
+echo "}" >> modules/custom/migrando/files/si8_glossario.json
+
+#####################################################################################################
+
 #       FILES
 #
-
 wget http://localhost:8888/it/api/si7_files_img.json -O modules/custom/migrando/files/si8_files_img.json
 sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_files_img.json
 echo "}" >> modules/custom/migrando/files/si8_files_img.json
@@ -80,15 +120,18 @@ wget http://localhost:8888/it/api/si7_files_no_img.json -O modules/custom/migran
 sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_files_no_img.json
 echo "}" >> modules/custom/migrando/files/si8_files_no_img.json
 
+#####################################################################################################
 
-#
-#       PAGINE STATICHE
-#
-
-#
 #       TERMINI DELLA TASSONOMIA
 #
 
 wget http://localhost:8888/en/api/si7_termini.json -O modules/custom/migrando/files/si8_termini.json
 sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_termini.json
 echo "}" >> modules/custom/migrando/files/si8_termini.json
+
+#####################################################################################################
+
+#        UTENTI 
+wget http://localhost:8888/it/api/si7_utenti.json -O modules/custom/migrando/files/si8_utenti.json
+sed -i -e '1i{"items":\' modules/custom/migrando/files/si8_utenti.json
+echo "}" >> modules/custom/migrando/files/si8_utenti.json
