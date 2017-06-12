12e7d4a5c8ae88738b905aed42ea5e587b3356a8
Sergio Durzu <sergio.durzu@gmail.com>
2017-06-12 08:42:16 +0200

    Removed unused reset-migrate.sh script

--- reset-migrate.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-drush pm-uninstall migrando -y; drush pm-uninstall migrate_tools -y; drush pm-uninstall migrate_plus -y; drush pm-uninstall migrate_drupal -y; drush pm-uninstall migrate -y
-
-drush en migrando -y
