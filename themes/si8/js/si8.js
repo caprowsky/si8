e30f65c05a6145b2b479af8bcac39689c17a51d0
Sergio Durzu <sergio.durzu@gmail.com>
2017-06-19 18:44:02 +0200

    Added glossario view

--- themes/si8/js/si8.js
+++ themes/si8/js/si8.js
@@ -1,3 +1,4 @@
 jQuery(function() {
     jQuery('article.page.full .field--name-field-box .field--item').matchHeight();
+    jQuery('.path-glossario .elenco-glossario .views-row').matchHeight();
 });
