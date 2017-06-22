9d74a80b0faf62e378087f9971fdf1a3b9ad20c8
Sergio Durzu <sergio.durzu@gmail.com>
2017-06-22 13:47:41 +0200

    Update procedimento confs

--- themes/si8/js/si8.js
+++ themes/si8/js/si8.js
@@ -1,4 +1,5 @@
 jQuery(function() {
     jQuery('article.page.full .field--name-field-box .field--item').matchHeight();
     jQuery('.path-glossario .elenco-glossario .views-row').matchHeight();
+    jQuery('.trova-procedimento-page .block-facets ul li a').matchHeight();
 });
