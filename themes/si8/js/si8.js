b64dd16b3fc8435df5e4e72ef11af3d0eb5182c5
Sergio Durzu <sergio.durzu@gmail.com>
2017-06-26 11:46:26 +0200

    Themized homepage

--- themes/si8/js/si8.js
+++ themes/si8/js/si8.js
@@ -2,4 +2,7 @@ jQuery(function() {
     jQuery('article.page.full .field--name-field-box .field--item').matchHeight();
     jQuery('.path-glossario .elenco-glossario .views-row').matchHeight();
     jQuery('.trova-procedimento-page .block-facets ul li a').matchHeight();
+    jQuery('.paragraph--type--notizie-in-evidenza .field--name-field-collegamenti > .field--item').matchHeight();
+
+
 });
