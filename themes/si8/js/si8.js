b47becdf59d444aecc4d8b72eb7d6761555a16af
Sergio Durzu <sergio.durzu@gmail.com>
2017-06-27 17:20:10 +0200

    Added field group and theme_change

--- themes/si8/js/si8.js
+++ themes/si8/js/si8.js
@@ -1,5 +1,5 @@
 jQuery(function() {
-    jQuery('article.page.full .field--name-field-box .field--item').matchHeight();
+    jQuery('.page.full .content > .field--name-field-box > .field--item').matchHeight();
     jQuery('.path-glossario .elenco-glossario .views-row').matchHeight();
     jQuery('.elenco-video .views-row').matchHeight();
     jQuery('.trova-procedimento-page .block-facets ul li a').matchHeight();
