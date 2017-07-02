7f509d77e1399867e06e2bc43acf68a49b8ae405
marco <m.piras@tsc-consulting.com>
2017-07-02 13:28:59 +0200

    Replicato comportamento main menu laterale

--- themes/si8/js/si8-menu.js
+++ themes/si8/js/si8-menu.js
@@ -10,3 +10,22 @@ jQuery("#edit-submit-ricerca-responsive").on("click", function () {
    }
     jQuery(".ricerca section").slideToggle();
 });
+
+jQuery('#hamburger-toggle').click(function() {
+    jQuery('body,html').animate({scrollTop:0},200);
+    jQuery('body').toggleClass('main-menu-opened');
+    jQuery('#block-menu-hamburger').toggleClass('active', 1000);
+});
+jQuery('#hamburger-close').on('click',function(e){
+    e.preventDefault();
+    jQuery('body').toggleClass('main-menu-opened');
+    jQuery('#block-menu-hamburger').toggleClass('active', 1000);
+});
+
+jQuery('.accordion-toggle').click(function(){
+    jQuery(this).siblings().slideToggle();
+    jQuery(this).toggleClass('collapsed');
+    jQuery(this).toggleClass('not-collapsed');
+    jQuery(this).children().toggleClass('fa-angle-down');
+    jQuery(this).children().toggleClass('fa-angle-up');
+});
