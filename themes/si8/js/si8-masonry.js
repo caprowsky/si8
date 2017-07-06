2b4e058376baf93f8bbba7261d7da3cd192c299c
marco <m.piras@tsc-consulting.com>
2017-07-07 00:55:44 +0200

    Masonry box primo livello, Video e Noizie in home

--- themes/si8/js/si8-masonry.js
+++ themes/si8/js/si8-masonry.js
@@ -2,29 +2,31 @@
 
 if(jQuery("body.path-frontpage").length == 0){
     jQuery(document).ready(function(){
-        //box primi livelli
-        jQuery('.field--items>div').attr('style','');
-        jQuery('.field--items').masonry({
-            itemSelector: '.field--items>div'
-        });
-
-        //video
         setTimeout(function(){
-            jQuery('body.path-video .griglia>div').attr('style','');
-            jQuery('body.path-video .griglia').masonry({
-                itemSelector: '.griglia>div'
+            //box primi livelli
+            jQuery('.field--items>div').attr('style','');
+            jQuery('.field--items').masonry({
+                itemSelector: '.field--items>div'
             });
-        },5000);
+
+            //video
+                jQuery('body.path-video .griglia>div').attr('style','');
+                jQuery('body.path-video .griglia').masonry({
+                    itemSelector: '.griglia>div'
+                });
+        },1500);
 
     });
 }
 //elementi in home
 else {
     jQuery(document).ready(function(){
-        //notizie
-        jQuery('body.path-frontpage .paragraph--type--notizie-in-evidenza .field--items>div').attr('style','');
-        jQuery('body.path-frontpage .paragraph--type--notizie-in-evidenza .field--items').masonry({
-            itemSelector: '.field--items>div'
+        setTimeout(function(){
+            //notizie
+            jQuery('body.path-frontpage .paragraph--type--notizie-in-evidenza .field--items>div').attr('style','');
+            jQuery('body.path-frontpage .paragraph--type--notizie-in-evidenza .field--items').masonry({
+                itemSelector: '.field--items>div'
         });
+        },1500);
     });
 }
