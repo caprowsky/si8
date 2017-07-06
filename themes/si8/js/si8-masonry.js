d4fac1fcfd8bbe88b250ac180f958e9b069d7d3a
marco <m.piras@tsc-consulting.com>
2017-07-07 00:43:36 +0200

    Masonry box primo livello e Video + Noizie in home #3

--- themes/si8/js/si8-masonry.js
+++ themes/si8/js/si8-masonry.js
@@ -5,9 +5,17 @@ if(jQuery("body.path-frontpage").length == 0){
         //box primi livelli
         jQuery('.field--items>div').attr('style','');
         jQuery('.field--items').masonry({
-            // options
             itemSelector: '.field--items>div'
         });
+
+        //video
+        setTimeout(function(){
+            jQuery('body.path-video .griglia>div').attr('style','');
+            jQuery('body.path-video .griglia').masonry({
+                itemSelector: '.griglia>div'
+            });
+        },5000);
+
     });
 }
 //elementi in home
@@ -16,12 +24,6 @@ else {
         //notizie
         jQuery('body.path-frontpage .paragraph--type--notizie-in-evidenza .field--items>div').attr('style','');
         jQuery('body.path-frontpage .paragraph--type--notizie-in-evidenza .field--items').masonry({
-            itemSelector: '.field--items>div'
-        });
-
-        //video
-        jQuery('body.path-frontpage .paragraph--type--video-in-evidenza .field--items>div').attr('style','');
-        jQuery('body.path-frontpage .paragraph--type--video-in-evidenza .field--items').masonry({
             itemSelector: '.field--items>div'
         });
     });
